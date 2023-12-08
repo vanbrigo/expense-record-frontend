6b3461b8bc2dfe595af2a05c142da2ef3c202257
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-08 15:33:05 +0100

    feat: add store file and import it in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from "./app/store.js";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from "redux-persist";
+
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
