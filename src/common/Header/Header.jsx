b542af37cf70f8990e2e17568f4287cfffe83f54
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-09 19:28:15 +0100

    feat: add login and sign in buttons

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,10 +1,21 @@
 import { Container } from "react-bootstrap"
 import "./Header.css"
+import { HeaderButton } from "../HeaderButton/HeaderButton"
 
 export const Header=()=>{
     return(
         <Container fluid className="headerDesign">
             <div className="logoHeader"></div>
+            <div className="headerBox">
+            <HeaderButton 
+            path='/'
+            title='SIGN IN'
+            />
+            <HeaderButton 
+            path='/'
+            title='LOG IN'
+            />
+            </div>
         </Container>
     )
 }
--- src/common/HeaderButton/HeaderButton.jsx
+++ src/common/HeaderButton/HeaderButton.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom'
 import './HeaderButton.css'
 
 
-export const HeaderButton=(path,title)=>{
+export const HeaderButton=({path,title})=>{
     const navigate=useNavigate()
     return(
         <div className='headerButtonDesign' onClick={()=>navigate(path)}>
