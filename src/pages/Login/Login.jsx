8687a81cfc3297894ebe75b9cf7496e19ec8fcc6
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-09 19:34:44 +0100

    feat: add variables to login function

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,19 @@
 import { Container } from 'react-bootstrap'
 import './Login.css'
+import { useState } from 'react'
 
 export const Login=()=>{
+    const [credentials, setCredentials]=useState({
+        email:'',
+        password:''
+    })
+
+    const [credencialesError,setCredencialesError] =useState({
+        emailError:"",
+        passwordError:""
+    })
+
+
     return(<Container fluid>
     </Container>)
 }
