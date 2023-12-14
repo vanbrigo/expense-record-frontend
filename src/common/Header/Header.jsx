80763a704b68fa3c86e474bf4d06f57790150418
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-14 12:44:19 +0100

    feat: add header buttons with routes to go to my balance, my record and my profile

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,12 +1,25 @@
 import { Container } from "react-bootstrap"
 import "./Header.css"
 import { HeaderButton } from "../HeaderButton/HeaderButton"
+import { useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { logout, userData } from "../../pages/userSlice"
 
 export const Header=()=>{
+    const rdxCredentials=useSelector(userData)
+    const token=rdxCredentials.credentials.data.token
+    const navigate=useNavigate()
+    const nickname=rdxCredentials.credentials.data.data.nickname
+    const logOutMe = () => {
+        dispatch(logout( {credentials : ""}))
+      }
+      console.log(nickname)
     return(
         <Container fluid className="headerDesign">
-            <div className="logoHeader"></div>
+            <div className="logoHeader" onClick={()=>navigate('/')}></div>
             <div className="headerBox">
+            {!token 
+            ?(<>
             <HeaderButton 
             path='/register'
             title='SIGN IN'
@@ -15,6 +28,28 @@ export const Header=()=>{
             path='/login'
             title='LOG IN'
             />
+            </>) 
+            :(<>
+            <HeaderButton 
+            path='/'
+            title={`Welcome, ${nickname}`}
+            />
+            <HeaderButton 
+            path='/home'
+            title=' My balance'
+            />
+            <HeaderButton 
+            path='/'
+            title='My record'
+            />
+            <div onClick={logOutMe}>
+            <HeaderButton 
+            path={"/"} 
+            title={"Log out"} 
+            />  
+            </div>
+            </>)}
+            
             </div>
         </Container>
     )
