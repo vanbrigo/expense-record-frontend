41bfeebf75b79240d9d779b84db3db218c4d3681
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-19 13:15:35 +0100

    feat: add my record view

---
+++ src/pages/BalanceSheets/BalanceSheets.jsx
@@ -0,0 +1,9 @@
+import './BalanceSheets.css'
+import { Container } from 'react-bootstrap'
+
+export const BalanceSheets=()=>{
+    return(
+    <Container fluid className='balanceSheetsDesign'>
+
+    </Container>)
+}
