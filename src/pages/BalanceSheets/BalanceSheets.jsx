753297ed59b497d6a6fcfa6d6fde9b2b4b206c68
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-23 17:08:01 +0100

    fix: modify spaces in code

--- src/pages/BalanceSheets/BalanceSheets.jsx
+++ src/pages/BalanceSheets/BalanceSheets.jsx
@@ -17,104 +17,98 @@ import {
   } from 'chart.js'
 import { Bar } from 'react-chartjs-2'
 
-export const BalanceSheets=()=>{
-    const rdxCredentials=useSelector(userData)
-    const token=rdxCredentials.credentials.token
-    const [incomes, setIncomes]=useState([])
-    const [data, setData]=useState({})
-    const [balance, setBalance]=useState()
-    const [date,setDate]=useState()
-
-    ChartJS.register(
-        CategoryScale,
-        LinearScale,
-        BarElement,
-        Title,
-        Tooltip,
-        Legend
-    )
-    const options = {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top',
-          },
-          title: {
-            display: true,
-            text: `Balance: ${balance}`,
-          },
-        },
-      }
-    useEffect(()=>{
-        if(incomes.length===0){
-            getAllIncomes(token)
-            .then(result=>{
-                const allIncomes=result.data.data
-                const dateIncomes= allIncomes.map(income=>{
-                    return(dayjs(income.date).format('MMMM-YYYY'))
-                })
-                const incomesSet= new Set(dateIncomes)
-                setIncomes(incomesSet)
-            })
-            .catch(error=>console.log(error))
-        }
-    },[incomes])
-    useEffect(()=>{
-        if(date){
-            const month=dayjs(date).format('MM')
-            const year= dayjs(date).format('YYYY')
-            getBalanceByDate(month,year,token)
-            .then(result=>{
-                const dataResult=result.data.data
-                const dataUpdated={
-                    labels:[date],
-                    datasets: [
-                        {
-                          label: 'Incomes',
-                          data: [dataResult.incomes],
-                          backgroundColor: 'rgb(22, 136, 121)',
-                        },
-                        {
-                          label: 'Expenses',
-                          data: [dataResult.expenses],
-                          backgroundColor: 'rgb(77, 22, 136)',
-                        },
-                      ],
-                }
-                console.log(typeof(dataResult.expenses))
-                setData(dataUpdated)
-                setBalance(dataResult.balance)
-            })
-            .catch(error=>console.log(error))
-        }
-    },[date])
-    
-    const functionHandler = (e) => {
-        setDate(e.target.value)
+export const BalanceSheets = () => {
+  const rdxCredentials = useSelector(userData);
+  const token = rdxCredentials.credentials.token;
+  const [incomes, setIncomes] = useState([]);
+  const [data, setData] = useState({});
+  const [balance, setBalance] = useState();
+  const [date, setDate] = useState();
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: `Balance: ${balance}`,
+      },
+    },
+  };
+  useEffect(() => {
+    if (incomes.length === 0) {
+      getAllIncomes(token)
+        .then((result) => {
+          const allIncomes = result.data.data;
+          const dateIncomes = allIncomes.map((income) => {
+            return dayjs(income.date).format("MMMM-YYYY");
+          });
+          const incomesSet = new Set(dateIncomes);
+          setIncomes(incomesSet);
+        })
+        .catch((error) => console.log(error));
     }
-    return(
-    <Container fluid className='balanceSheetsDesign'>
-        {incomes.size>0
-        ?(<>
-        <select name='date' onChange={functionHandler}>
-            <option>Select a date</option>
-            {Array.from(incomes).map((element,index)=>{
-                return(
-                    <option key={index}>{element}</option>
-                )
-            })}
-        </select>
-        {data && data.labels && data.datasets &&
-        <div className='oldBalanceBox'>
-        {/* <div className='balanceBoxInside'>{dayjs(oneBalance.date).format('MMMM-YYYY')}</div> */}
-        <Bar data={data} options={options}/>
-        {/* <div className='balanceBoxInside balanceAmount'><span>Balance</span> {oneBalance.balance}</div>
-        <div className='balanceBoxInside'><span>Incomes</span> {oneBalance.incomes}</div>
-        <div className='balanceBoxInside'><span>Expenses</span> {oneBalance.expenses}</div> */}
-        </div>
-        }
-        </>)
-        :(<></>)}
+  }, [incomes]);
+  useEffect(() => {
+    if (date) {
+      const month = dayjs(date).format("MM");
+      const year = dayjs(date).format("YYYY");
+      getBalanceByDate(month, year, token)
+        .then((result) => {
+          const dataResult = result.data.data;
+          const dataUpdated = {
+            labels: [date],
+            datasets: [
+              {
+                label: "Incomes",
+                data: [dataResult.incomes],
+                backgroundColor: "rgb(22, 136, 121)",
+              },
+              {
+                label: "Expenses",
+                data: [dataResult.expenses],
+                backgroundColor: "rgb(77, 22, 136)",
+              },
+            ],
+          };
+          setData(dataUpdated);
+          setBalance(dataResult.balance);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [date]);
 
-    </Container>)
-}
+  const functionHandler = (e) => {
+    setDate(e.target.value);
+  };
+  return (
+    <Container fluid className="balanceSheetsDesign">
+      {incomes.size > 0 ? (
+        <>
+          <select name="date" onChange={functionHandler}>
+            <option>Select a date</option>
+            {Array.from(incomes).map((element, index) => {
+              return <option key={index}>{element}</option>;
+            })}
+          </select>
+          {data && data.labels && data.datasets && (
+            <div className="oldBalanceBox">
+              <Bar data={data} options={options} />
+            </div>
+          )}
+        </>
+      ) : (
+        <></>
+      )}
+    </Container>
+  );
+};
