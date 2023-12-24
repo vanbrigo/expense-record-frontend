4f72e4fa100808188366d2ecebe62750fd55b82e
MacBook-Air-de-Vanessa <vanessabritogonzalez@gmail.com>
2023-12-24 15:41:47 +0100

    style: add footer to home view

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,22 @@
 import './Home.css'
-import { Container } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 
 export const Home=()=>{
     return(<Container fluid className='homeDesign'>
+        <Row className='homeFirstRow'>
         <div className='leftBox'></div>
         <div className='centerBox'>
             <span className='firstLine'>Managing your finances has never been so simple and efficient.<span className='secondtLine'> The only app web that gets your money into shape.</span></span>
             
         </div>
-        <div></div>
+        </Row>
+        <Row className='homeFooterRow'>
+            <div className='footerBox'></div>
+            <div className='footerBox'>
+            <img width="55" height="55" src="https://img.icons8.com/glyph-neue/64/1A1A1A/twitter-circled.png" alt="twitter-circled"/>
+            <img width="52" height="52" src="https://img.icons8.com/ios-filled/50/1A1A1A/circled-envelope.png" alt="circled-envelope"/>
+            <img width="55" height="55" src="https://img.icons8.com/glyph-neue/64/000000/facebook-circled.png" alt="facebook-circled"/>
+            </div>
+        </Row>
     </Container>)
 }
